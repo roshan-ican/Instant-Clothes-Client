cffd3089a3596424864e361514950b819d1b3675
charged ahead <roshan-ican>
2021-08-07 17:23:39 +0530

    uncomplete

--- screens/User/UserScreen.js
+++ screens/User/UserScreen.js
@@ -1,14 +1,91 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, Image, View, TouchableOpacity } from "react-native";
 
 const UserScreen = () => {
   return (
-    <View>
-      <Text>UserScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.header}></View>
+      <Image style={styles.avatar} />
+      <View style={styles.body}>
+        <Text style={styles.name}>User Name</Text>
+        <View style={styles.infoContainer}>
+          <Text>User Name</Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <Text>Phone</Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <Text>Email</Text>
+        </View>
+        <TouchableOpacity style={styles.infoContainer}>
+          <Text>Orders</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default UserScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#00BFFF",
+    height: 200,
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom: 10,
+    alignSelf: "center",
+    position: "absolute",
+    marginTop: 130,
+  },
+  name: {
+    fontSize: 22,
+    color: "#FFFFFF",
+    fontWeight: "600",
+  },
+  body: {
+    marginTop: 40,
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: "center",
+    padding: 30,
+  },
+  name: {
+    fontSize: 28,
+    color: "#696969",
+    fontWeight: "600",
+  },
+  info: {
+    fontSize: 16,
+    color: "#00BFFF",
+    marginTop: 10,
+  },
+  infoContainer: {
+    marginTop: 10,
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+    backgroundColor: "#00BFFF",
+  },
+  buttonContainer: {
+    marginTop: 10,
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+    backgroundColor: "#00BFFF",
+  },
+});
