fe65f730f8f6a03a5e0c68ff3594c52fcbb5e5f6
Ibrahim <ibrahimfayyaz26@gmail.com>
2021-08-07 16:29:56 +0500

    Map Screen complete from noe on

--- screens/Map/MapScreen.js
+++ screens/Map/MapScreen.js
@@ -1,6 +1,21 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Platform,
+  Image,
+} from "react-native";
 import MapView from "../../components/MapView";
+import { Marker, Callout } from "react-native-maps";
+import { WebView } from "react-native-webview";
+
+const { width, height } = Dimensions.get("window");
+
+let Im;
+
+Platform.OS == "android" ? (Im = WebView) : (Im = Image);
 
 const MapScreen = () => {
   return (
@@ -11,7 +26,26 @@ const MapScreen = () => {
         latitudeDelta: 0.007,
         longitudeDelta: 0.007,
       }}
-    />
+    >
+      <Marker
+        icon={require("../../assets/marker.png")}
+        coordinate={{
+          latitude: 32.51922279320279,
+          longitude: 74.51706916093826,
+        }}
+      >
+        <Callout>
+          <View style={{ width: width / 4, height: height / 6 }}>
+            <Text>Bata</Text>
+            <Im
+              style={{ width: width / 3, height: height / 10 }}
+              resizeMode="contain"
+              source={{ uri: "https://static.toiimg.com/photo/54996872.cms" }}
+            />
+          </View>
+        </Callout>
+      </Marker>
+    </MapView>
   );
 };
 
