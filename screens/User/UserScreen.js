4d535b113ffcef44c9143254627b2f7301aa2b47
charged ahead <roshan-ican>
2021-08-12 16:45:08 +0530

    user screen updated

--- screens/User/UserScreen.js
+++ screens/User/UserScreen.js
@@ -1,122 +1,174 @@
 import React from "react";
 import styled, { css } from "styled-components/native";
+import { StyleSheet, View, SafeAreaView, Button } from "react-native";
 import {
-  StyleSheet,
+  Avatar,
+  Title,
+  Caption,
   Text,
-  Image,
-  View,
-  TouchableOpacity,
-  Button,
-} from "react-native";
+  TouchableRipple,
+} from "react-native-paper";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: papayawhip;
-`;
-const Title = styled.Text`
-  font-size: 22px;
-  color: palevioletred;
-  font-weight: 500;
-`;
-const ButtonContainer = styled.TouchableOpacity`
-	width: 100px;
-  
-	height: 40px
-	padding: 12px;
-	border-radius: 10px;	
-	background-color: ${(props) => props.backgroundColor};
-`;
-
-const ButtonText = styled.Text`
-  font-size: 20px;
-  color: ${(props) => props.textColor};
-  text-align: center;
-`;
-
-const CustomButton = (props) => (
-  <ButtonContainer
-    onPress={() => alert("Hi, No Orders now")}
-    backgroundColor={props.backgroundColor}
-  >
-    <ButtonText textColor={props.textColor}>{props.text}</ButtonText>
-  </ButtonContainer>
-);
+import Share from "react-native-share";
+import { style } from "styled-system";
 
 const UserScreen = () => {
+  const myCustomShare = async () => {
+    const shareOptions = {
+      message:
+        "Order your clothes from FoodFinder App. I've already ordered more than 10 meals on it.",
+      url: files.appLogo,
+    };
+    try {
+      const ShareResponse = await Share.open(shareOptions);
+      console.log(JSON.stringify(ShareResponse));
+    } catch (error) {
+      console.log("Error => ", error);
+    }
+  };
+
   return (
-    <>
-      <Container>
-        <Title>User Name</Title>
-      </Container>
-      <Container>
-        <Title>Email</Title>
-      </Container>
-      <Container>
-        <Title>Phone</Title>
-      </Container>
-      <Container>
-        <Title>Address</Title>
-        <CustomButton
-          text="Orders"
-          textColor="#01d1e5"
-          backgroundColor="lavenderblush"
-        />
-      </Container>
-    </>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.userInfoSection}>
+        <View style={{ flexDirection: "row", marginTop: 15 }}>
+          <Avatar.Image
+            source={{
+              uri: "https://api.adorable.io/avatars/80/abott@adorable.png",
+            }}
+            size={80}
+          />
+          <View style={{ marginLeft: 20 }}>
+            <Title style={[styles.title, { marginTop: 15, marginBottom: 5 }]}>
+              Ibrahim
+            </Title>
+            <Caption style={styles.caption}>@ibrahim</Caption>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.userInfoSection}>
+        <View style={styles.row}>
+          <Icon name="map-marker-radius" color="#777777" size={20} />
+          <Text style={{ color: "#777777", marginLeft: 20 }}>
+            Sikkim, India{" "}
+          </Text>
+        </View>
+        <View style={styles.row}>
+          <Icon name="phone" color="#777777" size={20} />
+          <Text style={{ color: "#777777", marginLeft: 20 }}>
+            +91-900000009
+          </Text>
+        </View>
+        <View style={styles.row}>
+          <Icon name="email" color="#777777" size={20} />
+          <Text style={{ color: "#777777", marginLeft: 20 }}>
+            john_doe@email.com
+          </Text>
+        </View>
+      </View>
+      <View style={styles.infoBoxWrapper}>
+        <View
+          style={[
+            styles.infoBox,
+            {
+              borderRightColor: "#dddddd",
+              borderRightWidth: 1,
+            },
+          ]}
+        >
+          <Title>$100.50</Title>
+          <Caption>Wallat</Caption>
+        </View>
+        <View style={styles.infoBox}>
+          <Title>3</Title>
+          <Caption>Orders</Caption>
+        </View>
+      </View>
+      <View style={styles.menuWrapper}>
+        <TouchableRipple onPress={() => {}}>
+          <View>
+            <Icon name="heart-outline" color="#ff6347" size={25} />
+            <Text style={styles.menuItemText}>Favourites</Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={() => {}}>
+          <View style={styles.menuItem}>
+            <Icon name="credit-card" color="#FF6347" size={25} />
+            <Text style={styles.menuItemText}>Payment</Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={myCustomShare}>
+          <View style={styles.menuItem}>
+            <Icon name="share-outline" color="#FF6347" size={25} />
+            <Text style={styles.menuItemText}>Tell Your Friends</Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={() => {}}>
+          <View style={styles.menuItem}>
+            <Icon name="account-check-outline" color="#FF6347" size={25} />
+            <Text style={styles.menuItemText}>Support</Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={() => {}}>
+          <View style={styles.menuItem}>
+            <Icon name="settings-outline" color="#FF6347" size={25} />
+            <Text style={styles.menuItemText}>Settings</Text>
+          </View>
+        </TouchableRipple>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
-    backgroundColor: "#00BFFF",
-    height: 200,
-  },
-  name: {
-    fontSize: 22,
-    color: "#FFFFFF",
-    fontWeight: "600",
-  },
-  body: {
-    marginTop: 40,
-  },
-  bodyContent: {
+  container: {
     flex: 1,
-    alignItems: "center",
-    padding: 30,
   },
-  name: {
-    fontSize: 28,
-    color: "#696969",
+  userInfoSection: {
+    paddingHorizontal: 30,
+    marginBottom: 25,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 16,
+    fontWeight: "500",
+  },
+  row: {
+    flexDirection: "row",
+    marginBottom: 10,
+  },
+  infoBoxWrapper: {
+    borderBottomColor: "#dddddd",
+    borderBottomWidth: 1,
+    borderTopColor: "#dddddd",
+    borderTopWidth: 1,
+    flexDirection: "row",
+    height: 100,
+  },
+  infoBox: {
+    width: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  menuWrapper: {
+    marginTop: 10,
+  },
+  menuItem: {
+    flexDirection: "row",
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+  },
+  menuItemText: {
+    color: "#777777",
+    marginLeft: 20,
     fontWeight: "600",
-  },
-  info: {
     fontSize: 16,
-    color: "#00BFFF",
-    marginTop: 10,
-  },
-  infoContainer: {
-    marginTop: 10,
-    height: 45,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 20,
-    width: 250,
-    borderRadius: 30,
-    backgroundColor: "#00BFFF",
-  },
-  buttonContainer: {
-    marginTop: 10,
-    height: 45,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 20,
-    width: 250,
-    borderRadius: 30,
-    backgroundColor: "#00BFFF",
+    lineHeight: 26,
   },
 });
 export default UserScreen;
